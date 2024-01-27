9b1ff32a3eb4b7fc59d908cdf725e227afe67a4e
comidaparallevar <lyanosorio98@gmail.com>
2024-01-26 23:55:20 -0400

    changed new list variable

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    a_list = []
+    a_list = my_list[:]
     for index, value in enumerate(my_list):
         if value % 2 == 0:
             a_list[index] = True
