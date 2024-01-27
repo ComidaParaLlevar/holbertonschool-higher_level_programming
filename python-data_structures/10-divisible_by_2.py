eca172acc9cc0e795bc7163ac831cf7fc7013843
comidaparallevar <lyanosorio98@gmail.com>
2024-01-26 23:48:53 -0400

    added enumerate function

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     a_list = []
-    for i in my_list:
+    for i in enumerate(my_list):
         if i % 2 == 0:
             a_list[i] = True
         else:
