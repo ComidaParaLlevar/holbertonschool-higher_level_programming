44c2b43f9c723b9ea343bc2c14e052072c56da97
comidaparallevar <lyanosorio98@gmail.com>
2024-01-26 21:00:42 -0400

    added 6-print_matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        c = 1
+        l = len(i)
+        for j in i:
+            if c == l:
+                print("{:d}".format(j), end='')
+            else:
+                print("{:d}".format(j), end=' ')
+            c += 1
