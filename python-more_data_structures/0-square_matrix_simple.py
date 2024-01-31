7a14e6d7c072a3dec13b78332371499cd86daf7f
comidaparallevar <lyanosorio98@gmail.com>
2024-01-31 10:09:01 -0400

    added append function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
         if matrix:
-            new_matrix = (list(map(lambda x: x**2, i)))
-    return new_matrix
+            new_matrix.append(list(map(lambda x: x**2, i)))
 
+    return new_matrix
