a160ea3621ed28418419b25fc3ebc41082009a62
comidaparallevar <lyanosorio98@gmail.com>
2024-01-31 15:34:13 -0400

    made copy of original list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     index = 0
-    new_list = my_list
+    new_list = my_list.copy()
     for i in my_list:
         if i == search:
             new_list[index] = replace
