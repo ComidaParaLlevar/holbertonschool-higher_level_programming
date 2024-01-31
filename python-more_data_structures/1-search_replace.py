a478864afde0b4c99c66d4af90aed4d9d7a2320b
comidaparallevar <lyanosorio98@gmail.com>
2024-01-31 15:29:43 -0400

    added 1-search_replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    index = 1
+    new_list = my_list
+    for i in my_list:
+        if i == search:
+            new_list[index] = replace
+            index += 1
+    return new_list
