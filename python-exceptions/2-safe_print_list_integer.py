510c1b7cf632e751618cfbab509f6fe2e09cfd55
comidaparallevar <lyanosorio98@gmail.com>
2024-02-01 14:54:04 -0400

    deleted new line

--- python-exceptions/2-safe_print_list_integer.py
+++ python-exceptions/2-safe_print_list_integer.py
@@ -5,7 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
+        except (IndexError, ValueError, TypeError):
             continue
-    print("")
     return count
