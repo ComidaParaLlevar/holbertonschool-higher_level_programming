f26de440433d4fc2f70354b765269a3f6b190a9f
comidaparallevar <lyanosorio98@gmail.com>
2024-02-01 22:08:51 -0400

    added 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a square object"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """initialize method for storing attribute"""
+        self.__size = size
