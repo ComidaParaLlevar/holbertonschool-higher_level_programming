9723ffb89e4eda270181c1e6fec73c5f8ea0b9ca
comidaparallevar <lyanosorio98@gmail.com>
2024-02-02 09:25:54 -0400

    added size=0 to parameter

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,13 +4,13 @@
 
 class Square:
     """class Square contains method defining attribute size"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """initialize method for storing attribute"""
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
 
     def size_attr(self):
         return self.__size ** 2
