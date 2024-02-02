e567c988b0d23b93e97c98869490ebc2f9e8e9d6
comidaparallevar <lyanosorio98@gmail.com>
2024-02-02 12:58:35 -0400

    added decorato

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+
     """class Square contains method defining attribute size"""
     def __init__(self, size=0):
         """initialize method for storing attribute"""
@@ -12,9 +13,11 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
 
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
