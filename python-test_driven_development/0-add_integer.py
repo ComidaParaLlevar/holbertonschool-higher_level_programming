b7e5862e7fae11f1bc95e37ca8efcd47886c9e99
comidaparallevar <lyanosorio98@gmail.com>
2024-02-02 22:44:57 -0400

    added documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """adds two integers"""
     if not(isinstance(a, int)) or not(isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not(isinstance(b, int)) or not(isinstance(b, float)):
