ea0ad5512cdf25169b5298d33fa8974c33304eb1
comidaparallevar <lyanosorio98@gmail.com>
2024-02-15 19:54:41 -0400

    gave permission to rectangle, imported base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-    """ Module Rectangle
-    Inherits from Base"""
+""" Module Rectangle
+Inherits from Base"""
+from models.base import Base
 
 
-    class Rectangle(Base):
-        """ Rectangle representation."""
+class Rectangle(Base):
+    """ Rectangle representation."""
 
-        def __init__(self, width, height, x=0, y=0, id=None):
-            """initializes Rectangle object"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializes Rectangle object"""
 
-            self.__width = width
-            self.__height = height
-            self.x = x
-            self.y = y
-            super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
