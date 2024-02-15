2085ef55af35572324233afeca5b43fae2b8f18b
comidaparallevar <lyanosorio98@gmail.com>
2024-02-15 19:46:21 -0400

    added rectanle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+    """ Module Rectangle
+    Inherits from Base"""
+
+
+    class Rectangle(Base):
+        """ Rectangle representation."""
+
+        def __init__(self, width, height, x=0, y=0, id=None):
+            """initializes Rectangle object"""
+
+            self.__width = width
+            self.__height = height
+            self.x = x
+            self.y = y
+            super().__init__(id)
