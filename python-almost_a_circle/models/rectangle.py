5a646b185c260b43cbdeeb7168949e76dfc2a0f0
comidaparallevar <lyanosorio98@gmail.com>
2024-02-16 10:41:24 -0400

    put esceptions in setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -49,6 +49,10 @@ if __name__ != "__main__":
 
         @width.setter
         def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
             self.__width = value
 
         @property
@@ -58,6 +62,10 @@ if __name__ != "__main__":
 
         @height.setter
         def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be > 0")
             self.__height = value
 
         @property
@@ -67,6 +75,10 @@ if __name__ != "__main__":
 
         @x.setter
         def x(self, value):
+            if not isinstance(value, int):
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
             self.__x = value
 
         @property
@@ -76,4 +88,8 @@ if __name__ != "__main__":
 
         @y.setter
         def y(self, value):
+            if not isinstance(value, int):
+                raise TypeError("y must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
             self.__y = value
