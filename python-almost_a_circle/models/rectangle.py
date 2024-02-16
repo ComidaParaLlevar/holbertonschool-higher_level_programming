5b23a9d8986672d64955c43c3e159970de4cf1ef
comidaparallevar <lyanosorio98@gmail.com>
2024-02-15 20:08:44 -0400

    added documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,14 @@ class Rectangle(Base):
     """ Rectangle representation."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initializes Rectangle object"""
+        """initializes Rectangle object
+
+        Args:
+            - __width: width
+            - __height: height
+            - __x: position
+            - __y: position
+            - id: id"""
 
         self.__width = width
         self.__height = height
