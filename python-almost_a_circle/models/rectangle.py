b3492e7bcd75934d2ab8730ccc8b8a67478eb99d
comidaparallevar <lyanosorio98@gmail.com>
2024-02-17 19:25:50 -0400

    hardcoded each possible entry

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -109,11 +109,30 @@ if __name__ != "__main__":
 
         def update(self, *args, **kwargs):
             """ assigns an argument to each attribute """
-            attr = ["id", "width", "height", "x", "y"]
-            if len(args) > 0:
-                for i, j in enumerate(args):
-                    if i < len(attr):
-                        self.__setattr__(attr[i], j)
-                    else:
-                        for c, k in kwargs.items():
-                            self.__setattr__(c, k)
+            if args is not None and len(args) != 0:
+                if len(args) >= 1:
+                    if type(args[0]) != int and args[0] is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = args[0]
+                if len(args) > 1:
+                    self.width = args[1]
+                if len(args) > 2:
+                    self.height = args[2]
+                if len(args) > 3:
+                    self.x = args[3]
+                if len(args) > 4:
+                    self.y = args[4]
+                else:
+                    for key, value in kwargs.items():
+                        if key == "id":
+                            if type(value) != int and value is not None:
+                                raise TypeError("id must be an integer")
+                            self.id = value
+                        if key == "width":
+                            self.width = value
+                        if key == "height":
+                            self.height = value
+                        if key == "x":
+                            self.x = value
+                        if key == "y":
+                            self.y = value
