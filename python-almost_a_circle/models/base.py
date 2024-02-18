1c74579039d7c85c65f17857070d983faad06c45
comidaparallevar <lyanosorio98@gmail.com>
2024-02-17 22:00:42 -0400

    added to_json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ script lng """
 
+import json
 
 class Base():
     """This class will be the “base” of all other classes in this project.
@@ -20,3 +21,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ takes list of dictionaries and return json"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
