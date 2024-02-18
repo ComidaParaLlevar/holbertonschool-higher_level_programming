1de4960a8bf495a7a1d1f21a7405a6aab71c8129
comidaparallevar <lyanosorio98@gmail.com>
2024-02-17 20:09:14 -0400

    deleted exceptions

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -109,30 +109,26 @@ if __name__ != "__main__":
 
         def update(self, *args, **kwargs):
             """ assigns an argument to each attribute """
-            if args is not None and len(args) != 0:
-                if len(args) >= 1:
-                    if type(args[0]) != int and args[0] is not None:
-                        raise TypeError("id must be an integer")
-                    self.id = args[0]
-                if len(args) > 1:
-                    self.width = args[1]
-                if len(args) > 2:
-                    self.height = args[2]
-                if len(args) > 3:
-                    self.x = args[3]
-                if len(args) > 4:
-                    self.y = args[4]
-                else:
-                    for key, value in kwargs.items():
-                        if key == "id":
-                            if type(value) != int and value is not None:
-                                raise TypeError("id must be an integer")
-                            self.id = value
-                        if key == "width":
-                            self.width = value
-                        if key == "height":
-                            self.height = value
-                        if key == "x":
-                            self.x = value
-                        if key == "y":
-                            self.y = value
+            if args:
+                for k, v in enumerate(args):
+                    if k == 0:
+                        self.id = v
+                    elif k == 1:
+                        self.width = v
+                    elif k == 2:
+                        self.height = v
+                    elif k == 3:
+                        self.x = v
+                    else:
+                        self.y = v
+            else:
+                if "id" in kwargs:
+                    self.id = kwargs["id"]
+                if "width" in kwargs:
+                    self.width = kwargs["width"]
+                if "height" in kwargs:
+                    self.height = kwargs["height"]
+                if "x" in kwargs:
+                    self.x = kwargs["x"]
+                if "y" in kwargs:
+                    self.y = kwargs["y"]
